5ecd79a6732bac2b729e0bc032ebf075ef7f9808
sojip <31627694+sojip@users.noreply.github.com>
2020-02-08 19:28:41 +0100

    email mimetype ok

--- application.py
+++ application.py
@@ -18,27 +18,34 @@ def contact():
         phone = request.form['phone']
         email = request.form['email']
         content = request.form['message']
-        #send files to email address for test only
+        #send files to email address 
         port = 465
         password = "fullstack@145"
         smtp_server = "smtp.gmail.com"
         sender_email = "comptetestijawa@gmail.com"  
         receiver_email = "sojipquantin@gmail.com"
-        message = """\
-        Subject: New Contact From www.ijawatech.com
+        message = MIMEMultipart()
+        message["Subject"] = "New Message From ijawatech.com"
+        message["From"] = sender_email
+        message["To"] = receiver_email
+        text = """\
+            New message from ijatech.com : 
+            Name : """ + name + """
+            phone : """ + phone + """
+            email : """ + email  + """
+            message : """ + content + """
+            """
+        # Turn these into plain/html MIMEText objects
+        part1 = MIMEText(text, "plain")
 
-        This message is sent from www.ijawatech.com.
+        # Add HTML/plain-text parts to MIMEMultipart message
+        # The email client will try to render the last part first
+        message.attach(part1)
         
-        Name = """ + name + """
-        phone = """ + phone + """
-        email = """ + email + """
-        message = """ + content + """
-        """  
-       
         context = ssl.create_default_context()
         with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
             server.login(sender_email, password)
-            server.sendmail(sender_email, receiver_email, message)
+            server.sendmail(sender_email, receiver_email, message.as_string())    
         return jsonify(True)
     except:
         return jsonify(False) 
